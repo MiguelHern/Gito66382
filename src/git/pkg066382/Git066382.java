ff6dde52559a9d08f476ca15ea8483c08223d893
hdezp <hdezp@LAPTOP-192CGVHD>
2021-02-18 21:02:14 -0600

    Primer commit

---
+++ src/git/pkg066382/Git066382.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.pkg066382;
+
+/**
+ *
+ * @author Miguel Hernández
+ */
+public class Git066382 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Miguel Hernández";
+        String matricula = "66382";
+        System.out.println("Soy " + nombre + " y mi matrícula es " + 66382);
+        
+    }
+    
+}
